19ce33fdac15aa903fb6d2a5dc39985894523cdb
SoniVega53 <svegam@miumg.edu.gt>
2026-07-27 16:29:17 -0600

    proyecto base

---
+++ src/main/java/backend_equipo_bravo/analisis_sistema_II/AnalisisSistemaIiApplication.java
@@ -0,0 +1,13 @@
+package backend_equipo_bravo.analisis_sistema_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisisSistemaIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisisSistemaIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend_equipo_bravo/analisis_sistema_II/AnalisisSistemaIiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_equipo_bravo.analisis_sistema_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalisisSistemaIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
