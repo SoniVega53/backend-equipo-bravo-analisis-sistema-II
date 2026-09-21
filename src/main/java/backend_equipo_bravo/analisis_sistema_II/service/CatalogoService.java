eb222fef7a28d3c3ddcbbc397a2810d9ceda1e0a
SoniVega53 <svegam@miumg.edu.gt>
2026-09-21 11:19:18 -0600

    feat: add catalogos

--- src/main/java/backend_equipo_bravo/analisis_sistema_II/service/CatalogoService.java
+++ src/main/java/backend_equipo_bravo/analisis_sistema_II/service/CatalogoService.java
@@ -31,6 +31,33 @@ public class CatalogoService {
     @Autowired
     private MenuRepository menuRepository;
 
+    @Autowired
+    private EstadoCivilRepository estadoCivilRepository;
+
+    @Autowired
+    private StatusEmpleadoRepository statusEmpleadoRepository;
+
+    @Autowired
+    private TipoDocumentoRepository tipoDocumentoRepository;
+
+    @Autowired
+    private DepartamentoRepository departamentoRepository;
+
+    @Autowired
+    private PuestoRepository puestoRepository;
+
+    @Autowired
+    private PersonaRepository personaRepository;
+
+    @Autowired
+    private BancoRepository bancoRepository;
+
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    @Autowired
+    private PeriodoPlanillaRepository periodoPlanillaRepository;
+
     public List<SelectOptionDto> getEmpresas() {
         return empresaRepository.findAll().stream()
                 .map(e -> new SelectOptionDto(e.getIdEmpresa(), e.getNombre()))
@@ -82,6 +109,69 @@ public class CatalogoService {
     public List<SelectOptionDto> getMenus(Integer idModulo) {
         return menuRepository.findByIdModulo(idModulo).stream()
                 .map(m -> new SelectOptionDto(m.getIdMenu(), m.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+
+
+    public List<SelectOptionDto> getEstadosCiviles() {
+        return estadoCivilRepository.findAll().stream()
+                .map(e -> new SelectOptionDto(e.getIdEstadoCivil(), e.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getStatusEmpleados() {
+        return statusEmpleadoRepository.findAll().stream()
+                .map(s -> new SelectOptionDto(s.getIdStatusEmpleado(), s.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getTiposDocumentos() {
+        return tipoDocumentoRepository.findAll().stream()
+                .map(t -> new SelectOptionDto(t.getIdTipoDocumento(), t.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getDepartamentos() {
+        return departamentoRepository.findAll().stream()
+                .map(d -> new SelectOptionDto(d.getIdDepartamento(), d.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getPuestos() {
+        return puestoRepository.findAll().stream()
+                .map(p -> new SelectOptionDto(p.getIdPuesto(), p.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getPuestosDepartamento(Integer idDepartamento) {
+        return puestoRepository.findAll().stream()
+                .filter(p -> p.getIdDepartamento() != null && p.getIdDepartamento().equals(idDepartamento))
+                .map(p -> new SelectOptionDto(p.getIdPuesto(), p.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getPersonas() {
+        return personaRepository.findAll().stream()
+                .map(p -> new SelectOptionDto(p.getIdPersona(), p.getNombre() + " " + p.getApellido()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getBancos() {
+        return bancoRepository.findAll().stream()
+                .map(b -> new SelectOptionDto(b.getIdBanco(), b.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getEmpleados() {
+        return empleadoRepository.findAll().stream()
+                .map(e -> new SelectOptionDto(e.getIdEmpleado(), "Empleado ID: " + e.getIdEmpleado()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SelectOptionDto> getPeriodosPlanilla() {
+        return periodoPlanillaRepository.findAll().stream()
+                .map(p -> new SelectOptionDto(p.getAnio(), "Año: " + p.getAnio() + " - Mes: " + p.getMes()))
                 .collect(Collectors.toList());
     }
 }
